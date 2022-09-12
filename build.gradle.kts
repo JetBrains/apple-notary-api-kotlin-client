822ef475b2c756396d843b1b5bc64e15ed667d5e
Titouan Bion <titouan.bion@jetbrains.com>
2022-09-12 17:41:20 +0200

    Add release and publishing flows

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,14 @@ plugins {
     val kotlinVersion = "1.7.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
+    id("org.jetbrains.dokka") version "1.7.10"
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("maven-publish")
+    id("signing")
+    id("org.hildan.github.changelog") version "1.11.1"
 }
+
+group = "io.github.archangelx360"
 
 repositories {
     mavenCentral()
@@ -27,4 +34,73 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+changelog {
+    futureVersionTag = project.version.toString()
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+val dokkaJavadocJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Kotlin docs with Dokka into a Javadoc jar"
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaJavadoc)
+}
+
+nexusPublishing {
+    packageGroup.set("io.github.archangelx360")
+    repositories {
+        sonatype()
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            artifact(sourcesJar)
+            artifact(dokkaJavadocJar)
+
+            val githubUser: String by project
+            val githubSlug = "$githubUser/${rootProject.name}"
+            val githubRepoUrl = "https://github.com/$githubSlug"
+
+            pom {
+                name.set(project.name)
+                description.set(project.description)
+                url.set(githubRepoUrl)
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("ArchangelX360")
+                        name.set("Titouan BION")
+                        email.set("titouan.bion@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:$githubRepoUrl.git")
+                    developerConnection.set("scm:git:git@github.com:$githubSlug.git")
+                    url.set(githubRepoUrl)
+                }
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["maven"])
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "notary-api-kotlin-client"
+
+gradle.beforeProject {
+    val localPropertiesFile = rootDir.resolve("local.properties")
+    if (!localPropertiesFile.exists()) {
+        val template = rootDir.resolve("local.properties.template")
+        template.copyTo(localPropertiesFile)
+    }
+    val localProperties = java.util.Properties()
+    localProperties.load(localPropertiesFile.inputStream())
+    localProperties.forEach { (k, v) -> if (k is String) project.extra.set(k, v) }
+}
