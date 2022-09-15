e332acabc9116b73e9b9129b91f137aad8e5d238
Titouan Bion <titouan.bion@jetbrains.com>
2022-09-15 15:39:46 +0200

    Remove `Content-Type` setting from GET request
    This is not necessary and it made queries to `developerLogUrl` fail.

--- src/main/kotlin/io/github/archangelx360/NotaryClientV2.kt
+++ src/main/kotlin/io/github/archangelx360/NotaryClientV2.kt
@@ -14,9 +14,12 @@ import io.ktor.client.call.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import java.nio.file.Path
 import kotlin.io.path.fileSize
 import kotlin.io.path.inputStream
@@ -30,10 +33,6 @@ class NotaryClientV2(
             // Apple does not respect Accept header, so we work around to make Ktor still deserialize `application/octet-stream` as JSON
             serialization(ContentType.Application.OctetStream, DefaultJson)
         }
-        defaultRequest {
-            contentType(ContentType.Application.Json)
-            accept(ContentType.Application.Json)
-        }
     },
 ) {
     /**
@@ -44,6 +43,8 @@ class NotaryClientV2(
     suspend fun submitSoftware(request: NewSubmissionRequest): NewSubmissionResponse {
         val response = httpClient.post("$baseUrl/notary/v2/submissions") {
             withAppleAuthentication(credentials)
+            contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
             setBody(request)
             expectSuccess = true
         }
@@ -58,6 +59,7 @@ class NotaryClientV2(
     suspend fun getPreviousSubmissions(): SubmissionListResponse {
         val response = httpClient.get("$baseUrl/notary/v2/submissions") {
             withAppleAuthentication(credentials)
+            accept(ContentType.Application.Json)
             expectSuccess = true
         }
         return response.body()
@@ -106,6 +108,7 @@ class NotaryClientV2(
     suspend fun getSubmissionStatus(submissionId: String): SubmissionResponse {
         val response = httpClient.get("$baseUrl/notary/v2/submissions/$submissionId") {
             withAppleAuthentication(credentials)
+            accept(ContentType.Application.Json)
             expectSuccess = true
         }
         return response.body()
@@ -119,6 +122,7 @@ class NotaryClientV2(
     suspend fun getSubmissionLog(submissionId: String): Logs {
         val response = httpClient.get("$baseUrl/notary/v2/submissions/$submissionId/logs") {
             withAppleAuthentication(credentials)
+            accept(ContentType.Application.Json)
             expectSuccess = true
         }
         val body = response.body<SubmissionLogURLResponse>()
@@ -127,6 +131,14 @@ class NotaryClientV2(
         val logReponse = httpClient.get(url) {
             expectSuccess = true
         }
-        return logReponse.body()
+        // We consume the response as text other we are getting
+        // ```
+        // io.ktor.client.call.NoTransformationFoundException: No transformation found:
+        // class io.ktor.utils.io.ByteBufferChannel (Kotlin reflection is not available)
+        // -> class io.github.archangelx360.models.Logs (Kotlin reflection is not available)
+        // ```
+        // Probably due to an invalid response Content-Type.
+        val logs = logReponse.bodyAsText()
+        return Json.decodeFromString(logs)
     }
 }
